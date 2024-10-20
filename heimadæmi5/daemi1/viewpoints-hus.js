8bbc877045131dc7abb1b301ccdfbae9581eb9c3
Amaragance682 <esdeathxoli@gmail.com>
2024-10-20 18:52:11 +0000

    dmobidi

--- heimadæmi5/daemi1/viewpoints-hus.js
+++ heimadæmi5/daemi1/viewpoints-hus.js
@@ -12,7 +12,7 @@
 var canvas;
 var gl;
 
-// position of the track
+
 var TRACK_RADIUS = 100.0;
 var TRACK_INNER = 90.0;
 var TRACK_OUTER = 110.0;
@@ -28,13 +28,13 @@ var numTrackVertices  = 2*TRACK_PTS + 2;
 var numRoofVertices = 12;
 
 
-// variables for moving car
+
 var carDirection = 0.0;
 var carXPos = 100.0;
 var carYPos = 0.0;
 var height = 0.0;
 
-// current viewpoint
+
 var view = 1;
 
 var colorLoc;
@@ -53,7 +53,7 @@ var rotationY = 0;
 var movement = false;
 var origX, origY;
 
-// the 36 vertices of the cube
+
 var cVertices = [
     // front side:
     vec3( -0.5,  0.5,  0.5 ), vec3( -0.5, -0.5,  0.5 ), vec3(  0.5, -0.5,  0.5 ),
@@ -75,12 +75,12 @@ var cVertices = [
     vec3( -0.5, -0.5,  0.5 ), vec3( -0.5,  0.5,  0.5 ), vec3( -0.5,  0.5, -0.5 )
 ];
 
-// vertices for the roof
+
 var roofVertices = [
-    // one side
+
     vec3(  0.5, -0.5,  0.5 ), vec3(  0.5,  0.5,  0.5 ), vec3(  0.0,  0.5,  1.0 ),
     vec3(  0.0,  0.5,  1.0 ), vec3(  0.0, -0.5,  1.0 ), vec3(  0.5, -0.5,  0.5 ),
-    // one side
+
     vec3( -0.5,  0.5,  0.5 ), vec3( -0.5, -0.5,  0.5 ), vec3(  0.0, -0.5,  1.0 ),
     vec3(  0.0, -0.5,  1.0 ), vec3(  0.0,  0.5,  1.0 ), vec3( -0.5,  0.5,  0.5 )
 ];
@@ -88,7 +88,7 @@ var roofVertices = [
 
 
 
-// vertices of the track
+
 var tVertices = [];
 
 
@@ -130,17 +130,17 @@ window.onload = function init()
         );
     }
     
-    // VBO for the track
+
     trackBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, trackBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(tVertices), gl.STATIC_DRAW );
 
-    // VBO for the roof
+
     roofBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, roofBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(roofVertices), gl.STATIC_DRAW );
 
-    // VBO for the cube
+
     cubeBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cubeBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(cVertices), gl.STATIC_DRAW );
@@ -154,7 +154,7 @@ window.onload = function init()
     
     mvLoc = gl.getUniformLocation( program, "modelview" );
 
-    // set projection
+
     pLoc = gl.getUniformLocation( program, "projection" );
     proj = perspective( 50.0, 1.0, 1.0, 500.0 );
     gl.uniformMatrix4fv(pLoc, false, flatten(proj));
@@ -162,7 +162,7 @@ window.onload = function init()
     document.getElementById("Viewpoint").innerHTML = "1: Fjarlægt sjónarhorn";
     document.getElementById("Height").innerHTML = "Viðbótarhæð: "+ height;
 
-    // Event listener for keyboard
+
     window.addEventListener("keydown", function(e){
         switch( e.keyCode ) {
             case 49:	// 1: distant and stationary viewpoint
@@ -182,19 +182,19 @@ window.onload = function init()
                 view = 4;
                 document.getElementById("Viewpoint").innerHTML = "4: Sjónarhorn ökumanns";
                 break;
-            case 53:	// 5: drive around while looking at a house
+            case 53:	
                 view = 5;
                 document.getElementById("Viewpoint").innerHTML = "5: Horfa alltaf á eitt hús innan úr bílnum";
                 break;
-            case 54:	// 6: Above and behind the car
+            case 54:	
                 view = 6;
                 document.getElementById("Viewpoint").innerHTML = "6: Fyrir aftan og ofan bílinn";
                 break;
-            case 55:	// 7: from another car in front
+            case 55:	
                 view = 7;
                 document.getElementById("Viewpoint").innerHTML = "7: Horft aftur úr bíl fyrir framan";
                 break;
-            case 56:	// 8: from beside the car
+            case 56:	
                 view = 8;
                 document.getElementById("Viewpoint").innerHTML = "8: Til hliðar við bílinn";
                 break;
@@ -203,11 +203,11 @@ window.onload = function init()
                 document.getElementById("Viewpoint").innerHTML = "9: Horfa alltaf á eitt hús innan úr bílnum";
                 break;
             
-            case 38:    // up arrow
+            case 38:   
                 height += 2.0;
                 document.getElementById("Height").innerHTML = "Viðbótarhæð: "+ height;
                 break;
-            case 40:    // down arrow
+            case 40:   
                 height -= 2.0;
                 document.getElementById("Height").innerHTML = "Viðbótarhæð: "+ height;
                 break;
@@ -218,7 +218,7 @@ window.onload = function init()
 }
 
 
-// create the vertices that form the car track
+
 function createTrack() {
 
     var theta = 0.0;
@@ -232,13 +232,13 @@ function createTrack() {
 }
 
 
-// draw a house in location (x, y) of size size
+
 function house( x, y, size, mv ) {
 
     mv = mult( mv, translate( x, y, size/2 ) );
     mv = mult( mv, scalem( size, size, size ) );
 
-    // draw roof
+  
     gl.uniform4fv( colorLoc, RED );
     gl.bindBuffer( gl.ARRAY_BUFFER, roofBuffer );
     gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
@@ -246,7 +246,7 @@ function house( x, y, size, mv ) {
     gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
     gl.drawArrays( gl.TRIANGLE_STRIP, 0, numRoofVertices );
 
-    // draw the walls
+ 
     gl.uniform4fv( colorLoc, YELLOW );
     gl.bindBuffer( gl.ARRAY_BUFFER, cubeBuffer );
     gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
@@ -256,10 +256,10 @@ function house( x, y, size, mv ) {
 }
     
 
-// draw the circular track and a few houses (i.e. red cubes)
+
 function drawScenery( mv ) {
 
-    // draw track
+
     gl.uniform4fv( colorLoc, GRAY );
     gl.bindBuffer( gl.ARRAY_BUFFER, trackBuffer );
     gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
@@ -268,7 +268,7 @@ function drawScenery( mv ) {
     gl.drawArrays( gl.TRIANGLE_STRIP, 0, numTrackVertices );
 
 
-    // draw houses    
+  
     house(-20.0, 50.0, 5.0, mv);
     house(0.0, 70.0, 10.0, mv);
     house(20.0, -10.0, 8.0, mv);
@@ -281,24 +281,24 @@ function drawScenery( mv ) {
 }
 
 
-// draw car as two blue cubes
+
 function drawCar( mv ) {
 
-    // set color to blue
+ 
     gl.uniform4fv( colorLoc, BLUE );
     
     gl.bindBuffer( gl.ARRAY_BUFFER, cubeBuffer );
     gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
 
     var mv1 = mv;
-    // lower body of the car
+
     mv = mult( mv, scalem( 10.0, 3.0, 2.0 ) );
     mv = mult( mv, translate( 0.0, 0.0, 0.5 ) );
 
     gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
     gl.drawArrays( gl.TRIANGLES, 0, numCubeVertices );
 
-    // upper part of the car
+
     mv1 = mult( mv1, scalem( 4.0, 3.0, 2.0 ) );
     mv1 = mult( mv1, translate( -0.2, 0.0, 1.5 ) );
 
@@ -321,10 +321,10 @@ function render()
     var mv = mat4();
     switch( view ) {
         case 1:
-        // Distant and stationary viewpoint
+
         mv = lookAt(vec3(250.0, 0.0, 100.0 + height), vec3(0.0, 0.0, 0.0), vec3(0.0, 0.0, 1.0));
         
-        // Apply rotation based on mouse movement
+
         mv = mult(mv, rotateX(rotationX));
         mv = mult(mv, rotateY(rotationY));
 
@@ -335,7 +335,7 @@ function render()
         
         break;
 	case 2:
-	    // Static viewpoint inside the track; camera follows car
+
 	    mv = lookAt( vec3(75.0, 0.0, 5.0+height), vec3(carXPos, carYPos, 0.0), vec3(0.0, 0.0, 1.0 ) );
 	    drawScenery( mv );
 	    mv = mult( mv, translate(carXPos, carYPos, 0.0) );
@@ -343,7 +343,7 @@ function render()
 	    drawCar( mv );
 	    break;
 	case 3:
-	    // Static viewpoint outside the track; camera follows car
+
 	    mv = lookAt( vec3(125.0, 0.0, 5.0+height), vec3(carXPos, carYPos, 0.0), vec3(0.0, 0.0, 1.0 ) );
 	    drawScenery( mv );
 	    mv = mult( mv, translate(carXPos, carYPos, 0.0) );
@@ -351,7 +351,7 @@ function render()
 	    drawCar( mv );
 	    break;
 	case 4:
-	    // Driver's point of view.
+
 	    mv = lookAt( vec3(-3.0, 0.0, 5.0+height), vec3(12.0, 0.0, 2.0+height), vec3(0.0, 0.0, 1.0 ) );
 	    drawCar( mv );
 	    mv = mult( mv, rotateZ( carDirection ) );
@@ -359,7 +359,7 @@ function render()
 	    drawScenery( mv );
 	    break;
 	case 5:
-	    // Drive around while looking at a house at (40, 120)
+
 	    mv = rotateY( -carDirection );
 	    mv = mult( mv, lookAt( vec3(3.0, 0.0, 5.0+height), vec3(40.0-carXPos, 120.0-carYPos, 0.0), vec3(0.0, 0.0, 1.0 ) ) );
 	    drawCar( mv );
@@ -368,7 +368,7 @@ function render()
 	    drawScenery( mv );
 	    break;
 	case 6:
-	    // Behind and above the car
+
 	    mv = lookAt( vec3(-12.0, 0.0, 6.0+height), vec3(15.0, 0.0, 4.0), vec3(0.0, 0.0, 1.0 ) );
 	    drawCar( mv );
 	    mv = mult( mv, rotateZ( carDirection ) );
@@ -376,7 +376,7 @@ function render()
 	    drawScenery( mv );
 	    break;
 	case 7:
-	    // View backwards looking from another car
+
 	    mv = lookAt( vec3(25.0, 5.0, 5.0+height), vec3(0.0, 0.0, 2.0), vec3(0.0, 0.0, 1.0 ) );
 	    drawCar( mv );
 	    mv = mult( mv, rotateZ( carDirection ) );
@@ -384,7 +384,7 @@ function render()
 	    drawScenery( mv );
 	    break;
 	case 8:
-	    // View from beside the car
+
 	    mv = lookAt( vec3(2.0, 20.0, 5.0+height), vec3(2.0, 0.0, 2.0), vec3(0.0, 0.0, 1.0 ) );
 	    drawCar( mv );
 	    mv = mult( mv, rotateZ( carDirection ) );
@@ -392,7 +392,7 @@ function render()
 	    drawScenery( mv );
 	    break;
     case 9:
-        // Drive around while looking at a house at (-40, 140)
+
 	    mv = rotateY( -carDirection );
         mv = lookAt( vec3(125.0, 0.0, 5.0+height), vec3(carXPos, carYPos, 0.0), vec3(0.0, 0.0, 1.0 ) );
         
@@ -407,7 +407,7 @@ function render()
         movement = true;
         origX = e.offsetX;
         origY = e.offsetY;
-        e.preventDefault();  // Disable drag and drop
+        e.preventDefault();  
     });
     
     canvas.addEventListener("mouseup", function(e) {
@@ -419,8 +419,8 @@ function render()
             var deltaX = e.offsetX - origX;
             var deltaY = e.offsetY - origY;
             
-            // Update the rotation angles based on mouse movement
-            rotationX += deltaY * 0.5;  // Adjust sensitivity
+         
+            rotationX += deltaY * 0.5;  
             rotationY += deltaX * 0.5;
     
             origX = e.offsetX;
